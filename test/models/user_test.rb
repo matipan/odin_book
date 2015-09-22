c44be14147e1a4bcf560c4f64fe766426055565c
kriox26 <matias.pan26@gmail.com>
2015-09-22 13:09:05 -0300

    Minitest reporters

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+	@valid_user   = users(:example)
+	@invalid_user = users(:invalid_example)
+  end
+
+  test "User should be valid" do
+    
+  end
 end
