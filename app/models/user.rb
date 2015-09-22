9c6f9fb22ef176b1c44452c7c3c2e3bb36b5625c
kriox26 <matias.pan26@gmail.com>
2015-09-22 11:17:54 -0300

    Add fields to users, profile_image with paperclip

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Validations
+  validates_presence_of :name, :last_name
+end
