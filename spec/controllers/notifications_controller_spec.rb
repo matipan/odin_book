5aeec728b14a6468a2342ea66374dcccafecb9aa
kriox26 <matias.pan26@gmail.com>
2015-09-27 23:40:44 -0300

    Refactored home view

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe NotificationsController, type: :controller do
 
+  describe "POST #destroy" do
+    context 'redirection' do
+
+      it 'redirects to users page with a _request_ notification' do
+	  end
+
+    end
+  end
+
 end
