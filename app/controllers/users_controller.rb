585234e8a894832967bdd94fda4576b27582c401
kriox26 <matias.pan26@gmail.com>
2015-09-27 23:05:11 -0300

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [ :show ]
+
+  def show
+  end
+
+  private
+
+	def find_user
+	  @user = User.find(params[:id])
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :posts
+  resources :users, only: [ :show ]
   resources :notifications, only: [ :destroy, :index ]
   root "static_pages#home"
 end
