a013efb5464b199163ad1505ae1bc953b6b9cdca
kriox26 <matias.pan26@gmail.com>
2015-10-04 15:05:06 -0300

    First of feature spec

--- spec/features/posts_managment_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Posts managment", type: :feature do
-end
--- spec/features/posts_managments_spec.rb
+++ spec/features/posts_managments_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.feature "PostsManagments", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:matias) { create(:user) }
+  before :example do
+    visit new_user_session_path
+	fill_in "user_email", with: matias.email
+	fill_in "user_password", with: matias.password
+	click_button "Log in"
+	visit('/')
+  end
+
+  describe "Posts creation" do
+	context 'when a user submits a valid post' do
+      it 'adds the post to the home page' do
+		fill_in "post-body", with: "Some body of some post"
+		click_button "Post"
+		expect(page).to have_text("Some body of some post")
+      end
+    end
+
+	context 'when a user submits an invalid post' do
+	  it 'does not add the post to the home page' do
+		fill_in "post-body", with: " "
+		click_button "Post"
+		expect(page).not_to have_text("")
+	  end
+	end
+  end
+
 end
