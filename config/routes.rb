eb1e2ede055ee3f1488574d6d48874ae19053a0d
kriox26 <matias.pan26@gmail.com>
2015-10-04 20:38:04 -0300

    fix signup issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+	sessions: 'users/sessions',
+	registrations: 'users/registrations'
+  }
   resources :posts
   resources :users, only: [ :show ]
   resources :notifications, only: [ :destroy, :index ]
