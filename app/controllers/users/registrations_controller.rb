c63deedc75ef592c5906a56415f05cdaa7828fa3
kriox26 <matias.pan26@gmail.com>
2015-10-04 20:38:10 -0300

    Add permitted parameters

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -29,11 +29,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
 	def configure_sign_up_params
-	  devise_parameter_sanitizer.for(:sign_up) << :attribute
+	  devise_parameter_sanitizer.for(:sign_up).push(:name, :last_name)
 	end
 
 	def configure_account_update_params
-	  devise_parameter_sanitizer.for(:account_update) << :attribute
+	  devise_parameter_sanitizer.for(:account_update).push(:name, :last_name)
 	end
 
 	def after_sign_up_path_for(resource)
