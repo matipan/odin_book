de016ca1c30885d7ae6ed4bca8f0a096dac853e8
kriox26 <matias.pan26@gmail.com>
2015-10-05 15:29:30 -0300

    test pending

---
+++ spec/features/likes_managments_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+require 'support/login_steps'
+
+RSpec.feature "LikesManagments", type: :feature do
+  include LoginSteps
+
+  pending "Implement feature test for liking and unliking"
+end
