de8197d5186b74b99439ee05c80b16535df2149a
kriox26 <matias.pan26@gmail.com>
2015-10-06 15:37:31 -0300

    Add ajax to creation and deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,27 @@
 class CommentsController < ApplicationController
+
   def create
+	@post = Post.find(params[:post_id])
+	@comment = current_user.comments.build(body: params[:comment][:body], post_id: @post.id)
+	respond_to do |format|
+	  if @comment.save
+		format.js
+		format.html { redirect_to root_url }
+	  end
+	end
   end
 
   def destroy
+	@comment = current_user.comments.find(params[:id])
+	respond_to do |format|
+	  if @comment.destroy
+		format.js
+		format.html { redirect_to root_url }
+	  end
+	end
   end
 
   def update
   end
+
 end
